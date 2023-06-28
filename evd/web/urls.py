0bbda686c202fc1abe3ee0e0bb196c4de398dbe2
KALKIVENKATESH2000 <kalkivenkateshdonga@gmail.com>
2023-06-28 12:37:56 +0530

    cms feature added

--- evd/web/urls.py
+++ evd/web/urls.py
@@ -3,6 +3,20 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
+    url(r'^content-admin/', contentAdmin, name='content-admin'),
+    url(r'^my_topic_ajax/', my_topic_ajax, name='my_topic_ajax'),
+    url(r'^upload_topics/', upload_topics, name='upload_topics'),
+    url(r'download_topics/', download_topic_data, name='download_topic_data'),
+    url(r'import-topics/', import_topics_data, name='import_topics_data'),
+    url(r'^my_subtopic_ajax/', my_subtopic_ajax, name='my_subtopic_ajax'),
+    url(r'^upload_subtopics/', upload_subtopics, name='upload_subtopics'),
+    url(r'download_subtopics/', download_subtopic_data, name='download_subtopic_data'),
+    url(r'import-subtopics/', import_subtopics_data, name='import_subtopics_data'),
+    url(r'content_details/', ContentDetailsView.as_view(), name='content_details'),
+    url(r'content_demand/', ContentDemandView.as_view(), name='content_demand'),
+    url(r'get_course/', get_course, name='get_course'),
+    url(r'^my_table_ajax/', my_table_ajax, name='my_table_ajax'),
+    
     url(r'^tttinfo/', tttinfo, name='tttinfo'),
     url(r'^get_time_table/', get_time_table, name='get_time_table'),
     url(r'^manage_tttinfo/', manage_tttinfo, name='manage_tttinfo'),
